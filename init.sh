bd4ab4dfc3f6d79d47905d81d7189adf59705d27
toshiaki <tsuda@tukumo.local>
2016-09-05 22:56:19 +0900

    initスクリプト追加

---
+++ init.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cd ../
+DATE=$(date +'%Y%m%d')
+mv trade trade.$DATE
+git clone https://github.com/toshiakisuda/trade.git
+cd trade
+bundle install --path vendor/bundler
+rake db:migrate
